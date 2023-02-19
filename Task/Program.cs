efefb9b86a4a109e5159ff0c2d3db6b0edd33ead
Mariya Nemzorova <nemzorova777@mail.ru>
2023-02-19 13:34:46 +0700

    Task solved

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,17 +1,18 @@
 ﻿Console.Write("Количество вводимых данных: ");
 int size = Convert.ToInt32(Console.ReadLine());
-string[] array = CreateArrayInt(size);
+string[] array = CreateArrayString(size);
 PrintArray(array);
-string[] result = ArrayLessThreeChar(array);
+int sizeNewArray = ArrLessThreeCharSize(array);
+string[] result = ArrayLessThreeChar(array, sizeNewArray);
 PrintArray(result);
 
 
-string[] CreateArrayInt(int arrSize)
+string[] CreateArrayString (int arrSize)
 {
     string[] arr = new string[arrSize];
     for (int i = 0; i < arr.Length; i++)
     {
-        Console.Write($"Введите числа (через Enter): ");
+        Console.Write("Введите данные через enter: ", i);
         arr[i] = Console.ReadLine();
     }
     return arr;
@@ -28,7 +29,28 @@ void PrintArray(string[] arr)
     Console.WriteLine("]");
 }
 
-static string[] ArrayLessThreeChar(string[] arr)
+int ArrLessThreeCharSize(string[] arr)
 {
-    
+    int newSize = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 3) 
+        newSize++;
+    }
+    return newSize;
+}
+
+string[] ArrayLessThreeChar(string[] arr, int newSize)
+{
+    int j = 0;
+    string[] newArr = new string[newSize];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 3)
+        {
+            newArr[j] = arr[i];
+            j++;
+        }
+    }
+    return newArr;
 }
